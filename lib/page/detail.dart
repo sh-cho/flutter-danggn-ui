b96c5198d707922582b1e3b940f0599a634276ad
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2023-01-23 16:03:01 +0900

    8까지 완료 (Carousel)

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class DetailContentView extends StatefulWidget {
@@ -10,25 +11,92 @@ class DetailContentView extends StatefulWidget {
 }
 
 class _DetailContentViewState extends State<DetailContentView> {
+  late List<String> imgList;
+  int _current = 0;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    imgList = List.generate(5, (index) => widget.data["image"]!);
+  }
+
   AppBar _appbarWidget() {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
+      leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back, color: Colors.white)),
       actions: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.share)),
-        IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        IconButton(
+            onPressed: () {}, icon: Icon(Icons.share, color: Colors.white)),
+        IconButton(
+            onPressed: () {}, icon: Icon(Icons.more_vert, color: Colors.white)),
       ],
     );
   }
 
   Widget _bodyWidget() {
-    return Hero(
-      tag: widget.data["cid"]!,
-      child: Image.asset(
-        widget.data["image"]!,
-        width: double.infinity,
-        fit: BoxFit.fill,
+    return Stack(
+      children: [
+        Hero(
+          tag: widget.data["cid"]!,
+          child: CarouselSlider(
+            items: imgList
+                .map((url) =>
+                    Image.asset(url, width: double.infinity, fit: BoxFit.fill))
+                .toList(),
+            options: CarouselOptions(
+              // height: 400,
+              aspectRatio: 1,
+              initialPage: 0,
+              enableInfiniteScroll: false,
+              viewportFraction: 1,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              },
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+
+          /// Center는 못씀. l r == 0이면 센터
+          left: 0,
+          right: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.asMap().entries.map((entry) {
+              return GestureDetector(
+                // onTap: () => _controller.animateToPage(entry.key),
+                child: Container(
+                  width: 10.0,
+                  height: 10.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white
+                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _bottomNavigationBarWidget() {
+    return SizedBox(
+      child: Container(
+        color: Colors.red,
       ),
+      height: 55,
     );
   }
 
@@ -37,6 +105,7 @@ class _DetailContentViewState extends State<DetailContentView> {
     return Scaffold(
       appBar: _appbarWidget(),
       body: _bodyWidget(),
+      bottomNavigationBar: _bottomNavigationBarWidget(),
       extendBodyBehindAppBar: true,
     );
   }
