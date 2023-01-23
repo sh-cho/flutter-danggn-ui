53c1738f60ca2a52d3154761431621f985eb0539
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2023-01-23 21:20:09 +0900

    11까지 완료 (Animation 등)

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -1,6 +1,11 @@
+import 'dart:math';
+
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dedent/dedent.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_danggn_ui/component/manner_temperature.dart';
+import 'package:flutter_danggn_ui/util.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailContentView extends StatefulWidget {
   final Map<String, String> data;
@@ -11,31 +16,81 @@ class DetailContentView extends StatefulWidget {
   State<DetailContentView> createState() => _DetailContentViewState();
 }
 
-class _DetailContentViewState extends State<DetailContentView> {
+class _DetailContentViewState extends State<DetailContentView>
+    with SingleTickerProviderStateMixin {
   late List<String> imgList;
   int _current = 0;
+  final ScrollController _scrollController = ScrollController();
+  double _scrollPosToAlpha = 0;
+
+  /// animation ?
+  late final AnimationController _animationController;
+  late final Animation _colorTween;
+
+  /// data
+  late final String imagePath;
+  late final String title;
+  late final String location;
+  late final String price;
+  late final String likes;
+  late final String cid;
+
+  @override
+  void initState() {
+    super.initState();
+
+    imagePath = widget.data["image"]!;
+    title = widget.data["title"]!;
+    location = widget.data["location"]!;
+    price = widget.data["price"]!;
+    likes = widget.data["likes"]!;
+    cid = widget.data["cid"]!;
+
+    _animationController = AnimationController(vsync: this);
+    _colorTween = ColorTween(begin: Colors.white, end: Colors.black)
+        .animate(_animationController);
+    _scrollController.addListener(() {
+      setState(() {
+        _scrollPosToAlpha = min(_scrollController.offset, 255.0);
+        _animationController.value = _scrollPosToAlpha / 255;
+      });
+    });
+  }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
 
-    imgList = List.generate(5, (index) => widget.data["image"]!);
+    imgList = List.generate(5, (index) => imagePath);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+
+    super.dispose();
+  }
+
+  Widget _makeIcon(IconData icon) {
+    return AnimatedBuilder(
+      animation: _colorTween,
+      builder: (context, child) => Icon(icon, color: _colorTween.value),
+    );
   }
 
   AppBar _appbarWidget() {
     return AppBar(
-      backgroundColor: Colors.transparent,
+      backgroundColor: Colors.white.withAlpha(_scrollPosToAlpha.toInt()),
       elevation: 0,
       leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back, color: Colors.white)),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: _makeIcon(Icons.arrow_back),
+      ),
       actions: [
-        IconButton(
-            onPressed: () {}, icon: Icon(Icons.share, color: Colors.white)),
-        IconButton(
-            onPressed: () {}, icon: Icon(Icons.more_vert, color: Colors.white)),
+        IconButton(onPressed: () {}, icon: _makeIcon(Icons.share)),
+        IconButton(onPressed: () {}, icon: _makeIcon(Icons.more_vert)),
       ],
     );
   }
@@ -44,7 +99,7 @@ class _DetailContentViewState extends State<DetailContentView> {
     return Stack(
       children: [
         Hero(
-          tag: widget.data["cid"]!,
+          tag: cid,
           child: CarouselSlider(
             items: imgList
                 .map((url) =>
@@ -121,21 +176,225 @@ class _DetailContentViewState extends State<DetailContentView> {
     );
   }
 
+  Widget _line() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      height: 1,
+      color: Colors.grey.withOpacity(0.3),
+    );
+  }
+
+  Widget _contentDetail() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 20),
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            "디지털/가전 · 22시간 전",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 12,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            dedent('''
+                누구든지 체포 또는 구속을 당한 때에는 즉시 변호인의 조력을 받을 권리를 가진다. 다만, 형사피고인이 스스로 변호인을 구할 수 없을 때에는 법률이 정하는 바에 의하여 국가가 변호인을 붙인다.
+
+                여자의 근로는 특별한 보호를 받으며, 고용·임금 및 근로조건에 있어서 부당한 차별을 받지 아니한다.
+
+                근로자는 근로조건의 향상을 위하여 자주적인 단결권·단체교섭권 및 단체행동권을 가진다.
+                '''),
+            style: TextStyle(
+              fontSize: 15,
+              height: 1.5,
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(
+            "채팅 3 · 관심 17 · 조회 275",
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 12,
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  Widget _reportButton() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+        // color: Colors.blue,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              "Report this post",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              // textAlign: TextAlign.left,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _otherCellContents() {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "판매자님의 판매 상품",
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "모두 보기",
+            style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _bodyWidget() {
-    return Column(
-      children: [
-        _makeSlider(),
-        _sellerSimpleInfo(),
+    /// nested -> SingleChildScrollView 대신 Custom
+    return CustomScrollView(
+      controller: _scrollController,
+      slivers: [
+        SliverList(
+          delegate: SliverChildListDelegate([
+            _makeSlider(),
+            _sellerSimpleInfo(),
+            _line(),
+            _contentDetail(),
+            _line(),
+            _reportButton(),
+            _line(),
+            _otherCellContents(),
+          ]),
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          sliver: SliverGrid(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+            ),
+            delegate: SliverChildListDelegate(
+              List.generate(20, (index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        color: Colors.grey,
+                        height: 120,
+                      ),
+                    ),
+                    Text("상품 제목", style: TextStyle(fontSize: 14)),
+                    Text(
+                      "금액",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                );
+              }).toList(),
+            ),
+          ),
+        ),
       ],
     );
   }
 
   Widget _bottomNavigationBarWidget() {
-    return SizedBox(
-      child: Container(
-        color: Colors.red,
-      ),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
       height: 55,
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: () {
+              print("하트 클릭");
+            },
+            child: SvgPicture.asset(
+              "assets/svg/heart_off.svg",
+              width: 25,
+              height: 25,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 15, right: 10),
+            width: 1,
+            height: 40,
+            color: Colors.grey.withOpacity(0.3),
+          ),
+          Column(
+            children: [
+              Text(
+                calcStringToWon(price),
+                style: TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "가격제안불가",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Color(0xfff08f4f),
+                  ),
+                  child: Text(
+                    "채팅으로 거래하기",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
